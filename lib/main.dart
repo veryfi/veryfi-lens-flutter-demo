fdd9845639e277678d9d633fa0dfee10f50ff7e8
Sebastian Valentierra <sebastianvalentierra@veryfi.com>
2023-12-07 17:44:00 -0500

    LP-816: Update UI match with lens

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,12 @@
-/// [dart-packages]
 import 'dart:async';
-import 'dart:io';
-
-/// [flutter-packages]
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
-/// [veryfi-packages]
 import 'package:veryfi/lens.dart';
-
-/// [third-party-packages]
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:lottie/lottie.dart';
 
 void main() async {
-  await dotenv.load();
+  await dotenv.load(fileName: ".env");
   runApp(MyApp());
 }
 
@@ -22,25 +15,26 @@ class MyApp extends StatefulWidget {
   _MyAppState createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
-  var widgetsList = <Widget>[];
+class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+  ExtractedData? _extractedData;
+  String? _jsonString;
+  bool _isLoading = false;
+  late TabController _tabController;
 
   @override
   void initState() {
     super.initState();
+    _tabController = TabController(length: 2, vsync: this);
     initPlatformState();
+    Veryfi.setDelegate(handleVeryfiEvent);
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
     Map<String, dynamic> credentials = {
-      'clientId':
-          dotenv.env['VERYFI_CLIENT_ID'] ?? 'XXXX', //Replace with your clientId
-      'userName':
-          dotenv.env['VERYFI_USERNAME'] ?? 'XXXX', //Replace with your username
-      'apiKey':
-          dotenv.env['VERYFI_API_KEY'] ?? 'XXXX', //Replace with your apiKey
-      'url': dotenv.env['VERYFI_URL'] ?? 'XXXX' //Replace with your url
+      'clientId': dotenv.env['VERYFI_CLIENT_ID'] ?? 'your_client_id',
+      'userName': dotenv.env['VERYFI_USERNAME'] ?? 'your_username',
+      'apiKey': dotenv.env['VERYFI_API_KEY'] ?? 'your_api_key',
+      'url': dotenv.env['VERYFI_URL'] ?? 'your_url'
     };
 
     Map<String, dynamic> settings = {
@@ -49,47 +43,48 @@ class _MyAppState extends State<MyApp> {
     };
 
     try {
-      Veryfi.initLens(credentials, settings);
+      await Veryfi.initLens(credentials, settings);
+      setState(() { _isLoading = false; });
     } on PlatformException catch (e) {
       setState(() {
-        var errorText = 'There was an error trying to initialize Lens:\n\n';
-        errorText += '${e.code}\n\n';
-        widgetsList.add(Text(errorText));
+        _isLoading = false;
+        showError('Error initializing Veryfi: ${e.message}');
       });
     }
+  }
+
+  void showError(String errorMessage) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text(errorMessage),
+      backgroundColor: Colors.red,
+    ));
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(useMaterial3: true),
-      darkTheme: ThemeData(useMaterial3: true),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Veryfi Lens Wrapper'),
-        ),
-        body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(24.0),
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  Center(
-                    child: ElevatedButton(
-                      onPressed: startListeningEvents,
-                      child: Text("Start Listening Events"),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Column(
-                    children: widgetsList,
-                  ),
-                ],
-              ),
-            ),
+          title: Text('Veryfi Lens Wrapper'),
+          bottom: TabBar(
+            controller: _tabController,
+            tabs: [
+              Tab(text: 'Extracted Data'),
+              Tab(text: 'JSON'),
+            ],
           ),
+        ),
+        body: Stack(
+          children: [
+            TabBarView(
+              controller: _tabController,
+              children: [
+                _buildExtractedDataView(),
+                _buildJsonDataView(),
+              ],
+            ),
+            if (_isLoading) _buildLoadingScreen(),
+          ],
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: onShowCameraPressed,
@@ -99,65 +94,103 @@ class _MyAppState extends State<MyApp> {
     );
   }
 
-  void startListeningEvents() {
-    Veryfi.setDelegate(handleVeryfiEvent);
-  }
-
   void onShowCameraPressed() async {
+    setState(() { _isLoading = true; });
     await Veryfi.showCamera();
   }
 
   void handleVeryfiEvent(LensEvent eventType, Map<String, dynamic> response) {
-    setState(() {
-      var veryfiResult = '${eventType.toString()}\n\n';
-      widgetsList.add(Text(
-        veryfiResult,
-        style: TextStyle(fontWeight: FontWeight.bold),
-      ));
-      veryfiResult = '${response.toString()}\n\n';
-      widgetsList.add(Text(veryfiResult));
-    });
-
-    if (eventType.index == 3) {
-      if (response["data"] != null &&
-          response["data"].toString().contains(".jpg")) {
-        var imagePath = response["data"].toString();
-        if (imagePath.contains("thumbnail")) {
-          widgetsList.add(Text(
-            "Thumbnail",
-            style: TextStyle(fontWeight: FontWeight.normal),
-          ));
-          widgetsList.add(
-            Center(
-              child: SizedBox(
-                height: 100,
-                width: 100,
-                child: Image.file(
-                  File(imagePath),
-                ),
-              ),
-            ),
-          );
-        } else {
-          widgetsList.add(Text(
-            "Original",
-            style: TextStyle(fontWeight: FontWeight.normal),
-          ));
-          widgetsList.add(
-            Image.file(
-              File(imagePath),
-            ),
-          );
-        }
+    if (eventType == LensEvent.update) {
+      var status = response['status'];
+      if (status == 'start') {
+        setState(() => _isLoading = true);
+      } else if (status == 'removed') {
+        setState(() => _isLoading = false);
+      }
+    } else if (eventType == LensEvent.success) {
+      setState(() {
+        _jsonString = response['data'].toString();
+        _extractedData = ExtractedData.fromJson(Map<String, dynamic>.from(response['data']));
+      });
+    } else if (eventType == LensEvent.close) {
+      var queueCount = response['queue_count'] ?? 0;
+      if (queueCount == 0) {
+        setState(() => _isLoading = false);
       }
     }
+  }
 
-    widgetsList.add(SizedBox(
-      height: 30,
-    ));
 
-    widgetsList.add(Divider(
-      thickness: 1,
-    ));
+  Widget _buildExtractedDataView() {
+    if (_extractedData == null) return Center(child: Text('No extracted data'));
+    return ListView(
+      children: [
+        ListTile(title: Text('ID: ${_extractedData?.id ?? "Not available"}')),
+        ListTile(title: Text('Invoice Number: ${_extractedData?.invoiceNumber ?? "Not available"}')),
+        ListTile(title: Text('Currency Code: ${_extractedData?.currencyCode ?? "Not available"}')),
+        ListTile(title: Text('Tax: ${_extractedData?.tax?.toStringAsFixed(2) ?? "Not available"}')),
+        ListTile(title: Text('Category: ${_extractedData?.category ?? "Not available"}')),
+        ListTile(title: Text('Image File Name: ${_extractedData?.imgFileName ?? "Not available"}')),
+        ListTile(title: Text('Reference: ${_extractedData?.reference ?? "Not available"}')),
+        ListTile(title: Text('Created Date: ${_extractedData?.createdDate ?? "Not available"}')),
+      ],
+    );
+  }
+
+  Widget _buildJsonDataView() {
+    if (_jsonString == null) return Center(child: Text('No JSON data'));
+    return SingleChildScrollView(
+      child: Text(_jsonString ?? 'No JSON data'),
+    );
+  }
+
+  Widget _buildLoadingScreen() {
+    return Container(
+      color: Colors.white,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Lottie.asset('assets/loading_animation.json'),
+            Text('Reading document...', style: TextStyle(fontSize: 20)),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ExtractedData {
+  String? id;
+  String? invoiceNumber;
+  String? currencyCode;
+  double? tax;
+  String? category;
+  String? imgFileName;
+  String? reference;
+  String? createdDate;
+
+  ExtractedData({
+    this.id,
+    this.invoiceNumber,
+    this.currencyCode,
+    this.tax,
+    this.category,
+    this.imgFileName,
+    this.reference,
+    this.createdDate,
+  });
+
+  factory ExtractedData.fromJson(Map<String, dynamic> json) {
+    return ExtractedData(
+      id: json['id']?.toString(),
+      invoiceNumber: json['invoice_number'],
+      currencyCode: json['currency_code'],
+      tax: json['tax'] != null ? double.tryParse(json['tax'].toString()) : null,
+      category: json['category'],
+      imgFileName: json['img_file_name'],
+      reference: json['reference_number'],
+      createdDate: json['created_date'],
+    );
   }
 }
