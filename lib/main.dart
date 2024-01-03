41305bfe3bc34292c9ad923dfc9de905eabb8d83
Sebastian Valentierra <sebastianvalentierra@veryfi.com>
2024-01-03 16:39:57 -0500

    LP-817: Update UI Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,163 +1,469 @@
-/// [dart-packages]
 import 'dart:async';
 import 'dart:io';
-
-/// [flutter-packages]
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
-/// [veryfi-packages]
 import 'package:veryfi/lens.dart';
-
-/// [third-party-packages]
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:lottie/lottie.dart';
+import 'package:flutter_json_view/flutter_json_view.dart';
 
 void main() async {
-  await dotenv.load();
+  await dotenv.load(fileName: ".env");
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  _MyAppState createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: MyHomePage(),
+      ),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
-  var widgetsList = <Widget>[];
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  bool showVeryfiResults = false;
+  ExtractedData? _extractedData;
+  bool _isLoading = false;
+  late TabController _tabController;
+  Map<String, dynamic> _eventData = {};
+
+  void _handleTabSelection() {
+    if (_tabController.indexIsChanging) {
+      setState(() {});
+    }
+  }
 
   @override
   void initState() {
     super.initState();
     initPlatformState();
+    _tabController = TabController(length: 2, vsync: this);
+    _tabController.addListener(_handleTabSelection);
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   Future<void> initPlatformState() async {
     Map<String, dynamic> credentials = {
-      'clientId':
-          dotenv.env['VERYFI_CLIENT_ID'] ?? 'XXXX', //Replace with your clientId
-      'userName':
-          dotenv.env['VERYFI_USERNAME'] ?? 'XXXX', //Replace with your username
-      'apiKey':
-          dotenv.env['VERYFI_API_KEY'] ?? 'XXXX', //Replace with your apiKey
-      'url': dotenv.env['VERYFI_URL'] ?? 'XXXX' //Replace with your url
+      'clientId': dotenv.env['VERYFI_CLIENT_ID'] ?? 'YourClientId',
+      'userName': dotenv.env['VERYFI_USERNAME'] ?? 'YourUserName',
+      'apiKey': dotenv.env['VERYFI_API_KEY'] ?? 'YourApiKey',
+      'url': dotenv.env['VERYFI_URL'] ?? 'YourUrl',
     };
 
     Map<String, dynamic> settings = {
       'blurDetectionIsOn': false,
-      'showDocumentTypes': true
+      'showDocumentTypes': true,
+      'documentTypes': ['long_receipt']
     };
 
     try {
-      Veryfi.initLens(credentials, settings);
+      await Veryfi.initLens(credentials, settings);
+      startListeningEvents();
     } on PlatformException catch (e) {
-      setState(() {
-        var errorText = 'There was an error trying to initialize Lens:\n\n';
-        errorText += '${e.code}\n\n';
-        widgetsList.add(Text(errorText));
-      });
+      print("Failed to initialize Veryfi: ${e.message}");
     }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(useMaterial3: true),
-      darkTheme: ThemeData(useMaterial3: true),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Veryfi Lens Wrapper'),
-        ),
-        body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(24.0),
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  Center(
-                    child: ElevatedButton(
-                      onPressed: startListeningEvents,
-                      child: Text("Start Listening Events"),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Column(
-                    children: widgetsList,
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: onShowCameraPressed,
-          child: Icon(Icons.camera_alt),
-        ),
-      ),
-    );
   }
 
   void startListeningEvents() {
     Veryfi.setDelegate(handleVeryfiEvent);
   }
 
-  void onShowCameraPressed() async {
-    await Veryfi.showCamera();
-  }
+  var widgetsList = <Widget>[];
 
   void handleVeryfiEvent(LensEvent eventType, Map<String, dynamic> response) {
     setState(() {
-      var veryfiResult = '${eventType.toString()}\n\n';
-      widgetsList.add(Text(
-        veryfiResult,
-        style: TextStyle(fontWeight: FontWeight.bold),
-      ));
-      veryfiResult = '${response.toString()}\n\n';
-      widgetsList.add(Text(veryfiResult));
+      String eventName = eventType.toString().split('.').last;
+      _eventData[eventName] ??= [];
+      _eventData[eventName].add({
+        'status': response['status'],
+        'msg': response['msg'],
+        'data': response['data'],
+      });
+      showVeryfiResults = true;
     });
 
-    if (eventType.index == 3) {
-      if (response["data"] != null &&
-          response["data"].toString().contains(".jpg")) {
-        var imagePath = response["data"].toString();
-        if (imagePath.contains("thumbnail")) {
-          widgetsList.add(Text(
-            "Thumbnail",
-            style: TextStyle(fontWeight: FontWeight.normal),
-          ));
-          widgetsList.add(
-            Center(
-              child: SizedBox(
-                height: 100,
-                width: 100,
-                child: Image.file(
-                  File(imagePath),
-                ),
-              ),
-            ),
-          );
-        } else {
-          widgetsList.add(Text(
-            "Original",
-            style: TextStyle(fontWeight: FontWeight.normal),
-          ));
-          widgetsList.add(
-            Image.file(
-              File(imagePath),
-            ),
-          );
-        }
+    if (eventType == LensEvent.update) {
+      var status = response['status'];
+      if (status == 'start') {
+        setState(() => _isLoading = true);
+      } else if (status == 'removed') {
+        setState(() => _isLoading = false);
+      }
+    } else if (eventType == LensEvent.success) {
+      setState(() {
+        _extractedData =
+            ExtractedData.fromJson(Map<String, dynamic>.from(response['data']));
+      });
+    } else if (eventType == LensEvent.close) {
+      var queueCount = response['queue_count'] ?? 0;
+      if (queueCount == 0) {
+        setState(() => _isLoading = false);
       }
     }
 
-    widgetsList.add(SizedBox(
+    widgetsList.add(const SizedBox(
       height: 30,
     ));
 
-    widgetsList.add(Divider(
+    widgetsList.add(const Divider(
       thickness: 1,
     ));
+  }
+
+  Widget buildVeryfiResultsList() {
+    return Stack(
+      children: [
+        Scaffold(
+          appBar: AppBar(
+            backgroundColor: const Color(0xFFE9ECE4),
+            bottom: PreferredSize(
+              preferredSize: const Size.fromHeight(50.0),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: _buildButton(0, 'Extracted Data'),
+                    ),
+                    const SizedBox(width: 10),
+                    Expanded(
+                      child: _buildButton(1, 'JSON'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          body: TabBarView(
+            controller: _tabController,
+            children: [
+              _buildExtractedDataView(),
+              _buildJsonDataView(),
+            ],
+          ),
+        ),
+        if (_isLoading) _buildLoadingScreen(),
+      ],
+    );
+  }
+
+  Widget _buildButton(int index, String text) {
+    return TextButton(
+      onPressed: () {
+        setState(() {
+          _tabController.animateTo(index); // Cambia el índice actual del TabController
+        });
+      },
+      style: TextButton.styleFrom(
+        backgroundColor: _tabController.index == index
+            ? const Color(0xFF00FA6C) // Fondo cuando está seleccionado
+            : const Color(0xFF002108), // Fondo cuando no está seleccionado
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+      ),
+      child: Text(
+        text,
+        style: TextStyle(
+          color: _tabController.index == index
+              ? const Color(0xFFE9ECE4) // Color cuando está seleccionado
+              : const Color(0xFF00FA6C), // Color cuando no está seleccionado
+        ),
+      ),
+    );
+  }
+
+
+
+  String getSettingsForDocumentType(String documentType) {
+    switch (documentType) {
+      case 'Lens for Receipts & Invoices':
+        return 'receipt';
+      case 'Lens for Long Receipts':
+        return 'long_receipt';
+      case 'Lens for Checks':
+        return 'check';
+      case 'Lens for Credit Cards':
+        return 'credit_card';
+      case 'Lens for Business Cards':
+        return 'business_card';
+      case 'Lens for OCR':
+        return 'code';
+      case 'Lens for W-2':
+        return 'w2';
+      case 'Lens for W-9':
+        return 'w9';
+      case 'Lens for Bank Statements':
+        return 'bank_statements';
+      case 'Lens for Barcodes':
+        return 'barcodes';
+      case 'Headless Receipts':
+        return 'receipt';
+      case 'Headless Credit Card':
+        return 'credit_card';
+      default:
+        return 'receipt';
+    }
+  }
+
+  void onShowCameraPressed(String documentType) async {
+    Map<String, dynamic> credentials = {
+      'clientId': dotenv.env['VERYFI_CLIENT_ID'] ?? 'YourClientId',
+      'userName': dotenv.env['VERYFI_USERNAME'] ?? 'YourUserName',
+      'apiKey': dotenv.env['VERYFI_API_KEY'] ?? 'YourApiKey',
+      'url': dotenv.env['VERYFI_URL'] ?? 'YourUrl',
+    };
+    var documentTypeResult = getSettingsForDocumentType(documentType);
+
+    Map<String, dynamic> settings = {
+      'blurDetectionIsOn': false,
+      'showDocumentTypes': true,
+      'documentTypes': [documentTypeResult]
+    };
+
+    setState(() {
+      Veryfi.initLens(credentials, settings);
+      startListeningEvents();
+      _isLoading = false;
+    });
+
+    await Veryfi.showCamera();
+  }
+
+  final List<Map<String, dynamic>> menuOptions = [
+    {
+      'title': 'Lens for Receipts & Invoices',
+      'icon': Icons.receipt_long,
+    },
+    {
+      'title': 'Lens for Long Receipts',
+      'icon': Icons.receipt,
+    },
+    {
+      'title': 'Lens for Checks',
+      'icon': Icons.account_balance_wallet,
+    },
+    {
+      'title': 'Lens for Credit Cards',
+      'icon': Icons.credit_card,
+    },
+    {
+      'title': 'Lens for Business Cards',
+      'icon': Icons.business_center,
+    },
+    {
+      'title': 'Lens for OCR',
+      'icon': Icons.text_fields,
+    },
+    {
+      'title': 'Lens for W-2',
+      'icon': Icons.library_books,
+    },
+    {
+      'title': 'Lens for W-9',
+      'icon': Icons.library_books,
+    },
+    {
+      'title': 'Lens for Bank Statements',
+      'icon': Icons.account_balance,
+    },
+    {
+      'title': 'Lens for Barcodes',
+      'icon': Icons.qr_code_scanner,
+    },
+  ];
+
+  final List<Map<String, dynamic>> headlessOptions = [
+    {
+      'title': 'Headless Receipts',
+      'icon': Icons.receipt_long,
+    },
+    {
+      'title': 'Headless Credit Card',
+      'icon': Icons.credit_card,
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFE9ECE4),
+      body: showVeryfiResults ? buildVeryfiResultsList() : buildMainMenu(),
+    );
+  }
+
+  Widget buildMainMenu() {
+    return Column(
+      children: [
+        const SizedBox(height: 20),
+        SizedBox(
+            height: 120, child: Image.asset('assets/ic_veryfi_logo_black.PNG')),
+        Card(
+          elevation: 10,
+          margin: const EdgeInsets.all(12.0),
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(5.0),
+          ),
+          child: SizedBox(
+            height: 550,
+            child: ListView(
+              physics: const NeverScrollableScrollPhysics(),
+              children: [
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Text(
+                      'Solutions',
+                      style: TextStyle(
+                          fontSize: 15.0, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                ...menuOptions.map((option) => Padding(
+                      padding: EdgeInsets.zero,
+                      child: ListTile(
+                        dense: true,
+                        leading: Icon(option['icon'], size: 24.0),
+                        title: Text(
+                          option['title'],
+                          style: const TextStyle(fontSize: 14.0),
+                        ),
+                        trailing: const Icon(Icons.settings, size: 20.0),
+                        onTap: () => onShowCameraPressed(option['title']),
+                      ),
+                    )),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildExtractedDataView() {
+    if (_extractedData == null) {
+      return const Center(child: Text('No extracted data'));
+    }
+    return ListView(
+      children: [
+        ListTile(title: Text('ID: ${_extractedData?.id ?? "Not available"}')),
+        ListTile(
+            title: Text(
+                'Invoice Number: ${_extractedData?.invoiceNumber ?? "Not available"}')),
+        ListTile(
+            title: Text(
+                'Currency Code: ${_extractedData?.currencyCode ?? "Not available"}')),
+        ListTile(
+            title: Text(
+                'Tax: ${_extractedData?.tax?.toStringAsFixed(2) ?? "Not available"}')),
+        ListTile(
+            title: Text(
+                'Category: ${_extractedData?.category ?? "Not available"}')),
+        ListTile(
+            title: Text(
+                'Image File Name: ${_extractedData?.imgFileName ?? "Not available"}')),
+        ListTile(
+            title: Text(
+                'Reference: ${_extractedData?.reference ?? "Not available"}')),
+        ListTile(
+            title: Text(
+                'Created Date: ${_extractedData?.createdDate ?? "Not available"}')),
+      ],
+    );
+  }
+
+  Widget _buildJsonDataView() {
+    if (_eventData.isEmpty) {
+      return const Center(child: Text('No JSON data'));
+    }
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        return SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              minWidth: constraints.maxWidth,
+            ),
+            child: JsonView.map(
+              _eventData,
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildLoadingScreen() {
+    return Container(
+      color: const Color(0xFFE9ECE4),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+                height: 120,
+                child: Image.asset('assets/ic_veryfi_logo_black.PNG')),
+            const Text('Reading document...', style: TextStyle(fontSize: 20)),
+            Lottie.asset('assets/loading_animation.json'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ExtractedData {
+  String? id;
+  String? invoiceNumber;
+  String? currencyCode;
+  double? tax;
+  String? category;
+  String? imgFileName;
+  String? reference;
+  String? createdDate;
+
+  ExtractedData({
+    this.id,
+    this.invoiceNumber,
+    this.currencyCode,
+    this.tax,
+    this.category,
+    this.imgFileName,
+    this.reference,
+    this.createdDate,
+  });
+
+  factory ExtractedData.fromJson(Map<String, dynamic> json) {
+    return ExtractedData(
+      id: json['id']?.toString(),
+      invoiceNumber: json['invoice_number'],
+      currencyCode: json['currency_code'],
+      tax: json['tax'] != null ? double.tryParse(json['tax'].toString()) : null,
+      category: json['category'],
+      imgFileName: json['img_file_name'],
+      reference: json['reference_number'],
+      createdDate: json['created_date'],
+    );
   }
 }
